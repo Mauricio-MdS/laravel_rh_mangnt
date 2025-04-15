38765871c6c1c91a32a6ce07159b1e53de5da150
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2025-04-15 19:01:39 -0300

    configura mailhog

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "EH MANGNT";
+Route::get('/email', function () {
+    Mail::raw('Mensagem de teste de RH MANGNT', function (Message $message) {
+        $message->to('teste@gmail.com')->subject('Bem-vindo ao RH MANGNT')->from('rh@thmangnt.com');
+    });
+    echo "Email enviado com sucesso.";
 });
