e6dc877d620fe4d67860d1b9c1257d5d537725a3
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2025-04-15 19:30:09 -0300

    cria relações entre os models

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Mail\Message;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
@@ -9,4 +10,9 @@ Route::get('/email', function () {
         $message->to('teste@gmail.com')->subject('Bem-vindo ao RH MANGNT')->from('rh@thmangnt.com');
     });
     echo "Email enviado com sucesso.";
+});
+
+Route::get('/admin', function () {
+    $admin = User::with('detail', 'department')->find(1);
+    dd($admin->toArray());
 });
