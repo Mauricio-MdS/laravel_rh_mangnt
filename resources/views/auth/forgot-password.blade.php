d7815794872cb0ba7a0a28a0c939464283da384a
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2025-04-16 16:16:08 -0300

    reseta senha

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -10,29 +10,39 @@
 
                 <!-- forgot password -->
                 <div class="card p-5">
+                    @if (!session('status'))
+                        <p>Para recuperar a sua senha, por favor indique o seu email. Irá receber um email com um link
+                            para
+                            recuperar a senha.</p>
 
-                    <p>Para recuperar a sua senha, por favor indique o seu email. Irá receber um email com um link para
-                        recuperar a senha.</p>
+                        <form action="{{ route('password.email') }}" method="post">
+                            @csrf
 
-                    <form action="{{ route('password.email') }}" method="post">
-                        @csrf
+                            <div class="mb-3">
+                                <label for="email">Email</label>
+                                <input type="email" class="form-control" id="email" name="email">
+                                @error('email')
+                                    <div class="text-danger">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
 
-                        <div class="mb-3">
-                            <label for="email">Email</label>
-                            <input type="email" class="form-control" id="email" name="email">
-                            @error('email')
-                                <div class="text-danger">
-                                    {{ $message }}
-                                </div>
-                            @enderror
+                            <div class="d-flex justify-content-between align-items-center">
+                                <a href="{{ route('login') }}">Já sei a minha senha?</a>
+                                <button type="submit" class="btn btn-primary px-4">Enviar email</button>
+                            </div>
+                        </form>
+                    @else
+                        <div class="text-center mb-5">
+                            <p>Se está registrado nesta plataforma, irlá receber um email com um link para recuperar a
+                                senha.</p>
+                            <p class="mb-5">Por favor verifique a sua caixa de correio.</p>
+                            <a href="{{ route('login') }}" class="btn btn-primary px-4">Voltar ao login</a>
                         </div>
+                    @endif
 
-                        <div class="d-flex justify-content-between align-items-center">
-                            <a href="{{ route('login') }}">Já sei a minha senha?</a>
-                            <button type="submit" class="btn btn-primary px-4">Enviar email</button>
-                        </div>
 
-                    </form>
 
                 </div>
 
