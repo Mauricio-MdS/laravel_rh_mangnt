fdf380dc1bff5c6a7281c2b710a20cf45b7fc06b
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2025-04-16 19:17:49 -0300

    menu lateral

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,5 @@
 <x-layout-app page-title="Home">
-    <h1 class="text-center my-5">DENTRO DA APP</h1>
+    @can('admin')
+        <h3 class="text-center">É o usuário Admin que está logado</h3>
+    @endcan
 </x-layout-app>
