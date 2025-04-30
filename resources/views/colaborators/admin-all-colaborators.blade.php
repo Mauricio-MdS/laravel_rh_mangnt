9d829ea74fe021daa434c8b4481bb307d67319fe
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2025-04-30 15:20:03 -0300

    corrige casos em que departamento foi excluido

--- resources/views/colaborators/admin-all-colaborators.blade.php
+++ resources/views/colaborators/admin-all-colaborators.blade.php
@@ -34,7 +34,7 @@
                                     <span class="badge bg-success">Yes</span>
                                 @endempty
                             </td>
-                            <td>{{ $colaborator->department->name }}</td>
+                            <td>{{ $colaborator->department->name ?? '-' }}</td>
                             <td>{{ $colaborator->role }}</td>
                             <td>{{ $colaborator->detail->admission_date }}</td>
                             <td>{{ $colaborator->detail->salary }} $</td>
