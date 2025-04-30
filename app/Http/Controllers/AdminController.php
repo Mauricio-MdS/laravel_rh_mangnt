0ac645e48ccb991fe20d68a78129ecfbcfd40dc9
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2025-04-30 20:00:53 -0300

    admin home

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,7 +14,58 @@ class AdminController extends Controller
             abort(403, 'You are not authorized to access this page');
         }
 
+        // collect information about the organization
+        $data = [];
+
+        // get total number of colaboratores
+        $data['total_colaborators'] = User::count();
+
+        // total colaborators deleted
+        $data['total_colaborators_deleted'] = User::onlyTrashed()->count();
+
+        // total salary for all colaborators
+        $data['total_salary'] = number_format(
+            User::with('detail')
+                ->get()
+                ->sum(function ($colaborator) {
+                    return $colaborator->detail->salary;
+                })
+            ,
+            2,
+            ',',
+            '.'
+        ) . '$';
+
+        // total colaborators by department
+        $data['total_colaborators_per_department'] = User::with('department')
+            ->get()
+            ->groupBy('department_id')
+            ->map(function ($department) {
+                return [
+                    'department' => $department->first()->department->name ?? '-',
+                    'total' => $department->count()
+                ];
+            });
+
+        // total salary by department
+        $data['total_salary_by_department'] = User::with('department', 'detail')
+            ->get()
+            ->groupBy('department_id')
+            ->map(function ($department) {
+                return [
+                    'department' => $department->first()->department()->name ?? '-',
+                    'total' => number_format(
+                        $department->sum(function ($colaborator) {
+                            return $colaborator->detail->salary;
+                        }),
+                        2,
+                        ',',
+                        '.'
+                    ) . '$'
+                ];
+            });
+
         // display admin home page
-        return view('home');
+        return view('home', compact('data'));
     }
 }
