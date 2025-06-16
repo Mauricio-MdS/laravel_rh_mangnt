d9bc916a189b07e7682b9e9d24219fe1fc8a392f
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2025-06-16 12:45:09 -0300

    adiciona teste para login de usuário administrador

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\User;
+
+
+
 it('display the login page when not logged in', function () {
 
     $result = $this->get('/')->assertRedirect('/login');
@@ -11,4 +15,26 @@ it('display the login page when not logged in', function () {
 it('display the recover password page correctly', function () {
     expect($this->get('/forgot-password')->status())->toBe(200);
     expect($this->get('/forgot-password')->content())->toContain('Já sei a minha senha?');
+});
+
+it('test if an admin user can login with success', function () {
+    User::insert([
+        'department_id' => 1,   // Administração
+        'name' => 'Administrador',
+        'email' => 'admin@rhmangnt.com',
+        'email_verified_at' => now(),
+        'password' => bcrypt('Aa123456'),
+        'role' => 'admin',
+        'permissions' => '["admin"]',
+        'created_at' => now(),
+        'updated_at' => now(),
+    ]);
+
+    $result = $this->post('/login', [
+        'email' => 'admin@rhmangnt.com',
+        'password' => 'Aa123456',
+    ]);
+
+    expect($result->status())->toBe(302);
+    expect($result->assertRedirect('/home'));
 });
