c5a09aae08abade378825bd98d5ef5472311069e
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2025-06-16 16:17:28 -0300

    adiciona teste para inserção de novo colaborador por usuário RH

--- TESTES.md
+++ TESTES.md
@@ -9,7 +9,7 @@
 >> testar processo de login com sucesso para o perfil de colaborador
 
 >> testar a inserção de um novo user rh com o perfil do admin
-- testar a inserção de um novo colaborador com o perfil do rh
+>> testar a inserção de um novo colaborador com o perfil do rh
 
 - testar se um admin, após login com sucesso, consegue ver a página de colaboradores rh
 - testar se, sem usuário logado, é possível aceder à página home
--- tests/Feature/CreateUserTest.php
+++ tests/Feature/CreateUserTest.php
@@ -38,6 +38,41 @@ it('tests if an admin can insert a new RH user', function () {
     ]);
 });
 
+it('tests if a RH user can insert a new colaborator user', function () {
+
+    addRhUser();
+    addDepartment('Administração');
+    addDepartment('Recursos Humanos');
+    addDepartment('Armazem');
+
+    $result = $this->post('/login', [
+        'email' => 'rh1@rhmangnt.com',
+        'password' => 'Aa123456',
+    ]);
+
+    expect(auth()->user()->role)->toBe('rh');
+
+    $this->post('/rh-users/management/create-colaborator', [
+        'name' => 'Colaborator 1',
+        'email' => 'colaborator1@gmail.com',
+        'select_department' => 3,
+        'address' => 'Rua 2',
+        'zip_code' => '1234-000',
+        'city' => '1234-City 2',
+        'phone' => '123456789',
+        'salary' => '1000.00',
+        'admission_date' => '2021-01-10',
+        'role' => 'colaborator',
+        'permissions' => '["colaborator"]',
+    ]);
+
+    // $this->assertDatabaseHas('users', [
+    //     'email' => 'colaborator1@gmail.com',
+    // ]);
+
+    expect(User::where('email', 'colaborator1@gmail.com')->exists())->toBeTrue();
+});
+
 function addDepartment($name)
 {
     Department::insert([
